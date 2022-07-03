5c1e3dcbcfb670aa784f3c7897262796a1fa6348
Dini <dineshnagulapally@gmail.com>
2022-07-03 08:58:20 -0500

    Getting bean from ConfigurationApplicationContext with @Component (without @Autowired)

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,12 +2,16 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringbootApplication.class, args);
+        Alien a = context.getBean(Alien.class);
+        a.show();
+
     }
 
 }
